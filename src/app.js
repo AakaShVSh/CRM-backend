f6b02fa2e5201d605965822d5dafc2643c9fb73a
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2026-06-06 23:46:29 +0530

    cors frontend url added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const customerController = require("./controllers/customer.controller");
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://crm-backend-p6mb.onrender.com"],
+  origin: ["http://localhost:5173", "https://crm-frontend-grjk.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,

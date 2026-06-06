458cb6aa2b3b9f84fba98c3daf2fe5c027a5d365
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2026-06-06 23:40:17 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ const app = express();
 const cors = require("cors");
 const customerController = require("./controllers/customer.controller");
 
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://crm-backend-p6mb.onrender.com"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions)); // Apply updated CORS settings
 
 app.use("/api", customerController);
 

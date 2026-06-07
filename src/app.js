4ca89bc44daaa6d251f3fbf1c9d136548233a8fb
AakaShVSh <97448935+AakaShVSh@users.noreply.github.com>
2026-06-07 19:31:40 +0530

    upd

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const customerController = require("./controllers/customer.controller");
 
 const corsOptions = {
   origin: ["http://localhost:5173", "https://crm-frontend-grjk.onrender.com"],
-  methods: ["GET", "POST", "Patch", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
